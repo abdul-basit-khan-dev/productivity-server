c4f2836e1fb7a46e034c0fa82ed1422250753763
meanstackBasit <basitkhanbasit640@gmail.com>
2023-06-14 17:27:54 +0500

    Authentication implementation
    Signed-off-by: meanstackBasit <basitkhanbasit640@gmail.com>

---
+++ controllers/auth.controller.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken')
+
+require('dotenv').config({ path: '.env.example' });
+
+const EMAIL = process.env.EMAIL;
+const PASSWORD = process.env.PASSWORD;
+
+const login =  (req, res) => {
+
+    const {email, password} = req.body
+
+    if (email == EMAIL && password == PASSWORD) {
+        const token = jwt.sign({email}, process.env.TOKEN_KEY, {expiresIn: "2h"});
+        
+        return res.status(200).json({
+            statusCode: 200,
+            msg: "Login Successful",
+            token,
+        });
+    }
+
+    return res.status(401).json({
+        statusCode: 401,
+        msg: "Invalid Credentials",
+        token: 'not-valid'
+    });
+}
+
+
+module.exports = { login };
